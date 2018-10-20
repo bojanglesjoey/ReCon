2dc7eecdfa2436b0661e58a161eacd63b4fb18cb
Joohoon Kim <ddjnkm@gmail.com>
2018-10-20 13:22:00 -0400

    manipulate variables and test graphing.

--- dataset_itpr.py
+++ dataset_itpr.py
@@ -10,6 +10,8 @@ from os.path import isfile, join
 import sys
 import re
 import csv
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class FormatNC():
@@ -23,13 +25,26 @@ class FormatNC():
                 gas_name = file.split("_")
                 self.d_names.update( {re.sub('\.nc$', '', gas_name[3]) : file} )
 
-        for gas in self.d_names.keys():
+        for gas in gases:
             self.read_nc_files(gas)
+        plt.show()
 
     def read_nc_files(self, gas):
-        dataset = Dataset(self.d_names[gas])
-        print gas + "\n"
-        print dataset.dimensions.keys()
+        dataset = Dataset(self.d_names[gas], mode='r')
+        print gas
+        gas_conc = dataset.variables[gas][:]
+        years = dataset.variables['year'][:]
+        months = dataset.variables['month'][:]
+        long = dataset.variables['longitude'][:]
+        lat = dataset.variables['latitude'][:]
+        self.plot_chart(gas, months, gas_conc)
+        print "\n"
+        dataset.close()
+        
+    def plot_chart(self, gas, dfx, dfy):
+        fig, ax = plt.subplots()
+        fig.canvas.set_window_title(gas)
+        ax.plot(dfx, dfy)
 
 if __name__ == '__main__':
     gases = []
