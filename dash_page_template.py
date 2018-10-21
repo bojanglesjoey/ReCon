4b7d02cefd8b39e028b48ba2c0f554341c59738e
Joey Yang <joey.yang1997@hotmail.com>
2018-10-21 03:55:05 -0400

    Added z-axis metrics and types of chart selections

--- dash_page_template.py
+++ dash_page_template.py
@@ -9,231 +9,204 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 # Boostrap CSS.
 app.css.append_css({'external_url': 'https://codepen.io/amyoshino/pen/jzXypZ.css'})
 
-app.config.suppress_callback_exceptions = True
+#title of web page
+app.title = 'Dash Page'
 
 app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
-])
+    html.Div([
+        html.Div([
+            html.H1(children='Climate Change Data Visualization', 
+                className = "nine columns"),
+            html.Img(
+                src="https://upload.wikimedia.org/wikipedia/en/thumb/6/6d/Csa-asc_logo.svg/1200px-Csa-asc_logo.svg.png",
+                className ="two columns",
+                style={
+                    'height': '15%',
+                    'width': '15%',
+                    'float': 'right',
+                    'position': 'relative',
+                    'margin-top': 10,
+                },
+            ),
+        ], className = "row"),
 
-index_page = html.Div([
-    dcc.Link('Go to Graphing Page', href='/page-2'),
-    html.Br(),
-    dcc.Link('Go to Home Page', href='/page-1'),
-])
-
-#title of web page
-app.title = 'ReCon'
-
-page_2_layout = html.Div(children=[
-    html.Div(children=[
-        html.H1(children='Climate Change Data Visualization', 
-            className = "nine columns",
-            style={'color' : 'white',
-                'fontSize' : 50,
-                'font-family' : 'Helvetica',
-                'font-size' : 64,
-                'background-image': 'url(https://raw.githubusercontent.com/bojanglesjoey/ReCon/master/banner.png)'}),
-        html.Img(
-            src="https://upload.wikimedia.org/wikipedia/en/thumb/6/6d/Csa-asc_logo.svg/1200px-Csa-asc_logo.svg.png",
-            className ="two columns",
-            style={
-                'height': '15%',
-                'width': '15%',
-                'float': 'right',
-                'position': 'relative',
-                'margin-top': 10,
-            },
-        ),
-    ], className = "row"),
-
-    html.Div(children=[
-        dcc.Checklist(
-            options=[
-                {'label': 'Display Moving Average', 'value': 'MOVAVG'}
-            ],
-            values=['MOVAVG'],
-            labelStyle={'display': 'inline-block'},
-        )
-    ], className = "row"),
-
-    html.Div(children=[
-        html.Div(children=[
-            dcc.Graph(
-                id='graph1',
+        html.Div([
+            dcc.Checklist(
+                id='checklist1',
+                options=[
+                    {'label': 'Display Moving Average', 'value': 'MOVAVG'}
+                ],
+                #note: use "values" property for checklists
+                values=['MOVAVG'],
+                labelStyle={'display': 'inline-block'},
             )
-        ], className = "eight columns"),
-    ], className = "row"),
+        ], className = "row"),
 
-    html.Div(children=[
-        html.Div(children='''
-            Choose a gas of interest.
-        '''),
+        html.Div([
+            html.Div([
+                dcc.Graph(
+                    id='graph1',
+                )
+            ], className = "eight columns"),
+        ], className = "row"),
 
-        dcc.Dropdown(
-            id='dropdown1',
-            options=[
-                {'label': 'Carbon Dioxide', 'value': 'CO2'},
-                {'label': 'Water Vapour', 'value': 'H2O'}
-            ],
-            #default value(s)
-            value=['CO2', 'H2O'],
-            multi=False,
-        ),
-    ], className = "row"),
+        html.Div([
+            html.Div([
+                html.Div('''
+                    Choose a gas of interest:
+                '''),
 
-    html.Div(children=[
-        html.Div(children='''
-            Select an x-axis metric.
-        '''),
+                dcc.Dropdown(
+                    id='dropdown1',
+                    options=[
+                        {'label':'C2H2','value':'C2H2'},
+                        {'label':'CO2','value':'CO2'},
+                        {'label':'HNO3','value':'HNO3'},
+                        {'label':'C2H6','value':'C2H6'},
+                        {'label':'COCl2','value':'COCl2'},
+                        {'label':'HNO4','value':'HNO4'},
+                        {'label':'CCl2F2','value':'CCl2F2'},
+                        {'label':'COClF','value':'COClF'},
+                        {'label':'N2','value':'N2'},
+                        {'label':'CCl3F','value':'CCl3F'},
+                        {'label':'COF2','value':'COF2'},
+                        {'label':'N2O5','value':'N2O5'},
+                        {'label':'CCl4','value':'CCl4'},
+                        {'label':'CO','value':'CO'},
+                        {'label':'N2O','value':'N2O'},
+                        {'label':'CF4','value':'CF4'},
+                        {'label':'H2CO','value':'H2CO'},
+                        {'label':'NO2','value':'NO2'},
+                        {'label':'CFC113','value':'CFC113'},
+                        {'label':'H2O2','value':'H2O2'},
+                        {'label':'NO','value':'NO'},
+                        {'label':'CH3Cl','value':'CH3Cl'},
+                        {'label':'H2O','value':'H2O'},
+                        {'label':'O2','value':'O2'},
+                        {'label':'CH3OH','value':'CH3OH'},
+                        {'label':'HCl','value':'HCl'},
+                        {'label':'O3','value':'O3'},
+                        {'label':'CH4','value':'CH4'},
+                        {'label':'HCN','value':'HCN'},
+                        {'label':'OCS','value':'OCS'},
+                        {'label':'CHF2Cl','value':'CHF2Cl'},
+                        {'label':'HCOOH','value':'HCOOH'},
+                        {'label':'ClONO2','value':'ClONO2'},
+                        {'label':'HF','value':'HF'}
+                    ],
+                    value=["C2H2","CO2","HNO3","C2H6","COCl2","HNO4","CCl2F2","COClF","N2","CCl3F","COF2","N2O5","CCl4","CO","N2O","CF4","H2CO","NO2","CFC113","H2O2","NO","CH3Cl","H2O","O2","CH3OH","HCl","O3","CH4","HCN","OCS","CHF2Cl","HCOOH","ClONO2","HF"],
+                    #multi=False,
+                ),
+            ], className = "six columns"),
 
-        dcc.Dropdown(
-            id='dropdown2',
-            options=[
-                {'label': 'Altitude', 'value': 'ALT'},
-                {'label': 'Latitude', 'value': 'LAT'},
-                {'label': 'Gas Concentration', 'value': 'CONC'},
-                {'label': 'Pressure', 'value': 'PRES'},
-                {'label': 'Time', 'value': 'TIME'}
-            ],
-            value=['ALT', 'LAT', 'CONC', 'PRES', 'TIME'],
-            multi=False,
-        ),
-    ], className = "row"),
+            html.Div([
+                html.Div('''
+                    Choose a type of graph:
+                '''),
 
-    html.Div(children=[
-        html.Div(children='''
-            Select a y-axis metric.
-        '''),
+                dcc.Dropdown(
+                    id='dropdown2',
+                    options=[
+                        {'label':'Scatter Plot','value':'SCAT'},
+                        {'label':'Surface Plot','value':'SURF'},
+                        {'label':'Heat Map','value':'HEAT'}
+                    ],
+                    value=['SCAT','SURF','HEAT'],
+                    #multi=False,
+                ),
+            ], className = "six columns"),
+        ], className = "row"),
 
-        dcc.Dropdown(
-            id='dropdown3',
-            options=[
-                {'label': 'Altitude', 'value': 'ALT'},
-                {'label': 'Latitude', 'value': 'LAT'},
-                {'label': 'Gas Concentration', 'value': 'CONC'},
-                {'label': 'Pressure', 'value': 'PRES'},
-                {'label': 'Time', 'value': 'TIME'}
-            ],
-            value=['ALT', 'LAT', 'CONC', 'PRES', 'TIME'],
-            multi=False,
-        ),
-    ], className = "row"),
-    html.Div(id='page-2-content'),
-    html.Br(),
-    dcc.Link('Go to Home Page', href='/'),
+        html.Div([
+            html.Div('''
+                Select an x-axis metric:
+            '''),
+
+            dcc.Dropdown(
+                id='dropdown3',
+                options=[
+                    {'label': 'Altitude', 'value': 'altitude'},
+                    {'label': 'Latitude', 'value': 'latitude'},
+                    {'label': 'Longitude', 'value': 'longitude'},
+                    {'label': 'Beta Angle', 'value': 'beta_angle'},
+                    {'label': 'Hour', 'value': 'hour'},
+                    {'label': 'Day', 'value': 'day'},
+                    {'label': 'Month', 'value': 'month'},
+                    {'label': 'Year', 'value': 'year'},
+                ],
+                value=['altitude','latitude','longitude','beta_angle','hour','day','month','year'],
+                #multi=False,
+            ),
+        ], className = "row"),
+
+        html.Div([
+            html.Div('''
+                Select a y-axis metric:
+            '''),
+
+            dcc.Dropdown(
+                id='dropdown4',
+                options=[
+                    {'label': 'Altitude', 'value': 'altitude'},
+                    {'label': 'Latitude', 'value': 'latitude'},
+                    {'label': 'Longitude', 'value': 'longitude'},
+                    {'label': 'Beta Angle', 'value': 'beta_angle'},
+                    {'label': 'Hour', 'value': 'hour'},
+                    {'label': 'Day', 'value': 'day'},
+                    {'label': 'Month', 'value': 'month'},
+                    {'label': 'Year', 'value': 'year'},
+                    {'label': 'Temperature', 'value': 'temperature'},
+                    {'label': 'Temperature Fit', 'value': 'temperature_fit'},
+                    {'label': 'Pressure', 'value': 'pressure'},
+                    {'label': 'Concentration', 'value': 'concentration'},
+                ],
+                value=['altitude','latitude','longitude','beta_angle','hour','day','month','year','temperature','temperature_fit','pressure','concentration'],
+                #multi=False,
+            ),
+        ], className = "row"),
+
+        html.Div([
+            html.Div('''
+                Select a z-axis metric:
+            '''),
+
+            dcc.Dropdown(
+                id='dropdown5',
+                options=[
+                    {'label': 'Altitude', 'value': 'altitude'},
+                    {'label': 'Latitude', 'value': 'latitude'},
+                    {'label': 'Longitude', 'value': 'longitude'},
+                    {'label': 'Beta Angle', 'value': 'beta_angle'},
+                    {'label': 'Hour', 'value': 'hour'},
+                    {'label': 'Day', 'value': 'day'},
+                    {'label': 'Month', 'value': 'month'},
+                    {'label': 'Year', 'value': 'year'},
+                    {'label': 'Temperature', 'value': 'temperature'},
+                    {'label': 'Temperature Fit', 'value': 'temperature_fit'},
+                    {'label': 'Pressure', 'value': 'pressure'},
+                    {'label': 'Concentration', 'value': 'concentration'},
+                ],
+                value=['altitude','latitude','longitude','beta_angle','hour','day','month','year','temperature','temperature_fit','pressure','concentration'],
+                #multi=False,
+            ),
+        ], className = "row"),
+    ])
 ])
 
-page_1_layout = html.Div(children=[
-    html.Div(children=[
-        html.H1(children='ReCon: Climate Change Research to Consumers', 
-            className = "twelve columns",
-            style={'color' : 'white',
-                'fontSize' : 50,
-                'font-family' : 'Helvetica',
-                'font-size' : 64,
-                'background-image': 'url(https://raw.githubusercontent.com/bojanglesjoey/ReCon/master/banner.png)'}),
-    ], className = "row"),
-
-    html.Div(children=[
-        html.Img(
-             src="https://fournews-assets-prod-s3b-ew1-aws-c4-pml.s3.amazonaws.com/media/2017/06/global-temperatures-nasa.jpg",
-             className ="twelve columns",
-             style={
-                 'height': '100%',
-                 'width': '100%',
-                 'float': 'middle',
-                 'position': 'relative',
-                 'margin-top': 10,
-             },
-         ),
-    ]),
-    dcc.Textarea(
-        value='Global temperatures are rising. This graph from Nasa shows changes in global temperatures over the years. The different lines show the data collected by separate research centres.',
-        style={'width': '100%'}
-    ),
-    html.Div(children=[
-        html.Img(
-             src="https://fournews-assets-prod-s3-ew1-nmprod.s3.amazonaws.com/media/2017/06/co2-nasa.jpg",
-             className ="twelve columns",
-             style={
-                 'height': '100%',
-                 'width': '100%',
-                 'float': 'middle',
-                 'position': 'relative',
-                 'margin-top': 10,
-             },
-         ),
-    ]),
-    dcc.Textarea(
-        value='The amount of CO2 in the earth’s atmosphere is far higher than at any point in at least the last 400,000 years. And it’s still rising, this Nasa graph shows. CO2 levels have not been this high for at least three million years.',
-        style={'width': '100%'}
-    ),
-
-    html.Div(children=[
-        html.Img(
-             src="https://fournews-assets-prod-s3-ew1-nmprod.s3.amazonaws.com/media/2017/06/carbon-emissions-global-final.png",
-             className ="twelve columns",
-             style={
-                 'height': '100%',
-                 'width': '100%',
-                 'float': 'middle',
-                 'position': 'relative',
-                 'margin-top': 10,
-             },
-         ),
-    ]),
-    dcc.Textarea(
-        value='Research by the International Energy Agency charts the amount of energy-related carbon dioxide emissions across the world. Emissions are starting to level off, thanks to increased use of renewable energy and improved technology.',
-        style={'width': '100%'}
-    ),
-
-    html.Div(children=[
-        html.Img(
-             src="https://fournews-assets-prod-s3b-ew1-aws-c4-pml.s3.amazonaws.com/media/2017/06/arctic-sea-ice-minimum-nasa.png",
-             className ="twelve columns",
-             style={
-                 'height': '100%',
-                 'width': '100%',
-                 'float': 'middle',
-                 'position': 'relative',
-                 'margin-top': 10,
-             },
-         ),
-    ]),
-    dcc.Textarea(
-        value='Actic ice caps are melting. This shows the minimum amount of ice recorded in the Arctic each year. It is declining at a rate of 13.3 per cent every decade, according to Nasa.',
-        style={'width': '100%'}
-    ),
-
-    html.Div(id='page-1-content'),
-    html.Br(),
-    dcc.Link('Go to Graphing Page', href='/page-2'),
-])
-
-@app.callback(dash.dependencies.Output('page-content', 'children'),
-              [dash.dependencies.Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/page-2':
-        return page_2_layout
-    else:
-        return page_1_layout 
-    # You could also return a 404 "URL not found" page here
-
-#callback for graph1
-'''@app.callback(
+@app.callback(
     dash.dependencies.Output('graph1', 'figure'),
-    [dash.dependencies.Input('Cities', 'values')])
-def update_graph(selector):
+    [dash.dependencies.Input('dropdown1', 'value')])
+def select_gas_data(selector):
     data = []
-    if 'SF' in selector:
-        data.append({'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'})
-    if 'MT' in selector:
-        data.append({'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'})
+    if type(selector) == str:
+        if 'CO2' in selector:
+            data.append({'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'line', 'name': 'CO2'})
+        if 'H2O' in selector:
+            data.append({'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'line', 'name': 'H2O'})
     figure = {
         'data': data,
         'layout': {
-            'title': 'Graph 1',
+            'title': 'Data Visualization',
             'xaxis' : dict(
                 title='x Axis',
                 titlefont=dict(
@@ -250,7 +223,7 @@ def update_graph(selector):
             ))
         }
     }
-    return figure'''
+    return figure
 
 if __name__ == '__main__':
     app.run_server(debug=True)
